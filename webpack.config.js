723cb3e9ff24233b4bd0bbf3c63bef885e7c38b9
JobertDev27 <jobertdev27@gmail.com>
2025-01-23 18:10:10 +0800

    basic webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/main.html"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/main.html",
+    }),
+  ],
+  module: {
+    rules: [
+        {
+            test: /\.html$/i,
+            loader: "html-loader",
+        },
+        {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+        },
+    ],
+  },
+};
