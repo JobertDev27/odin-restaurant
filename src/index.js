8f1c8fa0f4c9f9ea2c249df106a7c656df25b795
JobertDev27 <jobertdev27@gmail.com>
2025-01-25 19:06:38 +0800

    added menu and contact dom. also added landpage

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 import "./style.css";
+import { menu } from "./menu";
+import { contact } from "./contact";
+
+(function (){
+    const main = document.getElementById("content");
+    const menuBtn = document.getElementById("menu-btn");
+    const contactBtn = document.getElementById("contact-btn");
+
+    menuBtn.addEventListener("click", () => {
+        clear();
+        menu();
+    });
+
+    contactBtn.addEventListener("click", () => {
+        clear();
+        contact();
+    });
+
+    const clear = () => {
+        while(main.firstChild) {
+            main.removeChild(main.lastChild);
+        }
+    };
+})();
